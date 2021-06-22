77eaeb491a556bf7a94cdbb1176c0f5998126ff0
Ramilko37 <galyamdin@gmail.com>
2021-06-22 18:46:20 +0300

    customer margines fixed

--- src/components/body/customer/Customer.tsx
+++ src/components/body/customer/Customer.tsx
@@ -24,16 +24,17 @@ const Customer: React.FC = () => {
     return (<div className="
     lg:max-h-full lg:w-1/4
     lg:items-center
-    lg:mt-0
+    mt-0
+    items-start
     border-solid shadow-2xl rounded-lg
     flex flex-row-reverse
-    justify-center lg:flex-col md:flex-row sm:flex-row
+    lg:flex-col md:flex-row sm:flex-row
     space-y-6
     space-x-6
     mt-10
     ">
         <img
-            className="img-rounded border-2 border-yellow-600 w-32 h-32"
+            className="ml-12 mr-5 lg:ml-0 img-rounded border-2 border-yellow-600 w-32 h-32"
             src={customer.imageUrl}
             alt=""
         />
