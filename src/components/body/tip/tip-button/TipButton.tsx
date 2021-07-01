70fa69ff1f3f4183e9b67686e28361437907deaf
Ramilko37 <galyamdin@gmail.com>
2021-07-01 11:56:17 +0300

    button switched to span

--- src/components/body/tip/tip-button/TipButton.tsx
+++ src/components/body/tip/tip-button/TipButton.tsx
@@ -8,11 +8,11 @@ interface IProps {
 }
 
 const TipButton: React.FC<IProps> = ({amount, handleAmount, bgColor}) => {
-  return  (<button onClick={() => handleAmount(amount)}
+  return  (<span onClick={() => handleAmount(amount)}
             className={`w-16 h-8 sm:w-28 sm:h-10 bg-${bgColor}
-             font-bold py-2 px-4 rounded-3xl mr-1 sm:mr-4 last:mr-0 focus:outline-none`}>
+             font-bold py-2 px-4 rounded-3xl mr-1 sm:mr-4 last:mr-0 text-white text-center pb-2`}>
         {amount}
-    </button>)
+    </span>)
 }
 
 
