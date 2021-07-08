967d54d3f4ae64af71e3f28fb09d8186d8d2ffea
Ramil Galyamdin <galyamdin@gmail.com>
2021-07-08 10:30:49 +0300

    btns styles fixes

--- src/components/body/tip/tip-button/TipButton.tsx
+++ src/components/body/tip/tip-button/TipButton.tsx
@@ -10,8 +10,8 @@ interface IProps {
 const TipButton: React.FC<IProps> = ({amount, handleAmount, bgColor}) => {
   return  (<span onClick={() => handleAmount(amount)}
             className={`w-16 h-8 sm:w-28 sm:h-10 bg-${bgColor}
-             font-bold py-2 px-4 rounded-3xl mr-1 sm:mr-4 last:mr-0 text-white text-center pb-2`}>
-        {amount}
+             font-bold py-2 px-4 rounded-3xl mr-1 sm:mr-4 last:mr-0 tip-btn-font text-white text-center pb-2`}>
+        {amount + ' €'}
     </span>)
 }
 
