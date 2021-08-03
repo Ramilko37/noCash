b3b515fd2b4b7ed6ebc98091c9246d7b1703d429
Ramilko37 <galyamdin@gmail.com>
2021-08-03 17:58:46 +0300

    checkbox styled

--- src/components/body/review/radiobuttons/Checkbox.tsx
+++ src/components/body/review/radiobuttons/Checkbox.tsx
@@ -8,14 +8,14 @@ interface IProps {
 const Checkbox: React.FC<IProps> = ({handleChecked, checked}) => {
 
     return(
-        <div className="flex items-center mr-4 mb-4">
+        <div className="flex mb-4">
             <input id="radio7" type="checkbox" name="radio" className="hidden"
                    checked={checked} onChange={() => handleChecked()} />
-            <label htmlFor="radio7" className="flex items-center cursor-pointer">
-                <span className="w-6 h-4 inline-block rounded-full radio-btn-border"></span>
-                <p className="text-xs max-w-md ml-5">I want to cover transaction costs (5% of the tip amount) so that
-                    the employee receives the full amount</p>
+            <label htmlFor="radio7" className="flex cursor-pointer">
+                <span className="w-4 h-4 rounded-full radio-btn-border"></span>
             </label>
+            <p className="text-xs w-30 ml-2">I want to cover transaction costs (5% of the tip amount) so that
+                the employee receives the full amount</p>
         </div>
     )
 }
